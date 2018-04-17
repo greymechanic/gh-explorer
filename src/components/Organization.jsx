4e56666375784546b231192e69b233a8f21ac0fb
notandrewkaye <notandrewkaye@gmail.com>
2018-04-16 23:30:59 -0700

    interaction on repo click

--- src/components/Organization.jsx
+++ src/components/Organization.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {Query} from "react-apollo";
 import gql from "graphql-tag";
+import classnames from "classnames";
 import "./Organization.css";
 
 const GET_GH_ORGANIZATION = gql`
@@ -21,33 +22,53 @@ const GET_GH_ORGANIZATION = gql`
   }
 `;
 
-const Organization = ({organization}) => (
-  <Query
-    query={GET_GH_ORGANIZATION}
-    variables={{organization}}
-    notifyOnNetworkStatusChange
-  >
-    {({loading, error, data, refetch}) => {
-      if (loading) return "Loading...";
-      if (error) return `Error!: ${error}`;
-      return (
-        <div className="organization-wrapper">
-          <h2>{data.organization.name}</h2>
-          <small>Popular Repositories</small>
-          <ul>
-            {data.organization.repositories.edges.map((repo, i) => {
-              return (
-                <li key={i}>
-                  {repo.node.name}
-                  <span>{repo.node.stargazers.totalCount} stargazers</span>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      );
-    }}
-  </Query>
-);
+export default class Organization extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default Organization;
+    this.state = {highlightedRepo: ""};
+  }
+
+  showCommits = repositoryName => {
+    this.setState({highlightedRepo: repositoryName});
+  };
+
+  render() {
+    const {organization} = this.props;
+    return (
+      <Query
+        query={GET_GH_ORGANIZATION}
+        variables={{organization}}
+        notifyOnNetworkStatusChange
+      >
+        {({loading, error, data, refetch}) => {
+          if (loading) return "Loading...";
+          if (error) return `Error!: ${error}`;
+          return (
+            <div className="organization-wrapper">
+              <h2>{data.organization.name}</h2>
+              <small>Popular Repositories</small>
+              <ul>
+                {data.organization.repositories.edges.map((repo, i) => {
+                  return (
+                    <li
+                      key={i}
+                      className={classnames({
+                        active: repo.node.name === this.state.highlightedRepo,
+                      })}
+                      onClick={() => this.showCommits(repo.node.name)}
+                    >
+                      {repo.node.name}
+                      <span>{repo.node.stargazers.totalCount} stargazers</span>
+                      <span className="trigger">browse commits</span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
+}
