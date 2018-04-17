ec0fd36296533a85a635fecfccdcdf04da6b388d
notandrewkaye <notandrewkaye@gmail.com>
2018-04-16 23:00:36 -0700

    order by num stargazers

--- src/components/Organization.jsx
+++ src/components/Organization.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {Query} from "react-apollo";
 import gql from "graphql-tag";
+import "./Organization.css";
 
 const GET_GH_ORGANIZATION = gql`
   query Organization($organization: String!) {
@@ -10,6 +11,9 @@ const GET_GH_ORGANIZATION = gql`
         edges {
           node {
             name
+            stargazers {
+              totalCount
+            }
           }
         }
       }
@@ -24,15 +28,20 @@ const Organization = ({organization}) => (
     notifyOnNetworkStatusChange
   >
     {({loading, error, data, refetch}) => {
-      if (loading) return "loading";
+      if (loading) return "Loading...";
       if (error) return `Error!: ${error}`;
       return (
-        <div>
-          <h3>{data.organization.name}</h3>
-          <p>Repositories</p>
+        <div className="organization-wrapper">
+          <h2>{data.organization.name}</h2>
+          <small>Popular Repositories</small>
           <ul>
             {data.organization.repositories.edges.map((repo, i) => {
-              return <li key={i}>{repo.node.name}</li>;
+              return (
+                <li key={i}>
+                  {repo.node.name}
+                  <span>{repo.node.stargazers.totalCount} stargazers</span>
+                </li>
+              );
             })}
           </ul>
         </div>
